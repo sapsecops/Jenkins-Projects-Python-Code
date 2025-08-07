5e98a2ea2cd8b301ec8db3655559abb153ab7aaf
techizone <venkateshsap413@gmail.com>
2025-08-07 10:32:35 +0530

    Update MY Repo with SAPSECOPS

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+import socket
 from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    hostname = socket.gethostname()
+    try:
+        ip_address = socket.gethostbyname(hostname)
+    except socket.gaierror:
+        ip_address = 'Unavailable'
+    
+    return render_template('index.html', hostname=hostname, ip_address=ip_address)
